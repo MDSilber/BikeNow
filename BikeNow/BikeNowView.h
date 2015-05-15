04876c12f31986eead68782adc77ca99101df5db
Mason Silber <mdsilber1@gmail.com>
2015-05-15 14:52:38 -0400

    Added view file

---
+++ BikeNow/BikeNowView.h
@@ -0,0 +1,13 @@
+//
+//  BikeNowView.h
+//  BikeNow
+//
+//  Created by Mason Silber on 5/15/15.
+//  Copyright (c) 2015 Mason Silber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BikeNowView : UIView
+
+@end
---
+++ BikeNow/BikeNowView.m
@@ -0,0 +1,13 @@
+//
+//  BikeNowView.m
+//  BikeNow
+//
+//  Created by Mason Silber on 5/15/15.
+//  Copyright (c) 2015 Mason Silber. All rights reserved.
+//
+
+#import "BikeNowView.h"
+
+@implementation BikeNowView
+
+@end
