51902bdcce54ace74b39f717e9f764afe3351eea
Mason Silber <mdsilber1@gmail.com>
2015-05-15 17:22:50 -0400

    Added JSON parsing methods for the model

--- BikeNow/BikeStation.h
+++ BikeNow/BikeStation.h
@@ -8,6 +8,14 @@
 
 #import <Foundation/Foundation.h>
 
+typedef NS_ENUM(NSUInteger, StationCity) {
+    StationCityUnknown,
+    StationCityNYC,
+    StationCityPhiladelphia,
+    StationCitySF,
+    StationCityChicago
+};
+
 @import CoreLocation;
 
 @interface BikeStation : NSObject
@@ -15,11 +23,15 @@
 @property (nonatomic, readonly) NSString *addressCity;
 @property (nonatomic, readonly) NSString *addressState;
 @property (nonatomic, readonly) NSString *addressZipCode;
-@property (nonatomic, readonly) NSUInteger numberOfBikesAvailable;
+@property (nonatomic, readonly) NSUInteger bikesAvailable;
 @property (nonatomic, readonly) NSUInteger docksAvailable;
 @property (nonatomic, readonly) NSString *stationID;
 @property (nonatomic, readonly) NSString *stationName;
 @property (nonatomic, readonly) NSUInteger totalDocks;
 @property (nonatomic, readonly) CLLocationCoordinate2D coordinate;
 @property (nonatomic, readonly) BOOL inService;
+@property (nonatomic, readonly) StationCity stationCity;
+
++ (BikeStation *)stationForJSON:(NSDictionary *)json stationCity:(StationCity)stationCity;
+
 @end
--- BikeNow/BikeStation.m
+++ BikeNow/BikeStation.m
@@ -10,4 +10,58 @@
 
 @implementation BikeStation
 
++ (BikeStation *)stationForJSON:(NSDictionary *)json stationCity:(StationCity)stationCity
+{
+    return [[self alloc] initWithJSON:json stationCity:stationCity];
+}
+
+- (instancetype)initWithJSON:(NSDictionary *)json stationCity:(StationCity)stationCity
+{
+    self = [super init];
+    
+    if (self) {
+        _stationCity = StationCityPhiladelphia;
+        
+        if (stationCity == StationCityPhiladelphia) {
+            [self _setUpForPhiladelphiaStationWithJSON:json];
+        } else {
+            [self _setUpForNonPhiladelphiaStationWithJSON:json];
+        }
+    }
+    
+    return self;
+}
+
+- (void)_setUpForPhiladelphiaStationWithJSON:(NSDictionary *)json
+{
+    NSArray *coordinates = json[@"geometry"][@"coordinates"];
+    if (coordinates) {
+        _coordinate = CLLocationCoordinate2DMake([[coordinates firstObject] doubleValue], [[coordinates lastObject] doubleValue]);
+    }
+    
+    NSDictionary *properties = json[@"properties"];
+    if (properties) {
+        _addressStreet = properties[@"addressStreet"];
+        _addressCity = properties[@"addressCity"];
+        _addressState = properties[@"addressState"];
+        _addressZipCode = properties[@"addressZipCode"];
+        _bikesAvailable = [properties[@"bikesAvailable"] integerValue];
+        _docksAvailable = [properties[@"docksAvailable"] integerValue];
+        _stationID = properties[@"kioskID"];
+        _inService = [properties[@"kioskPublicStatus"] isEqualToString:@"Active"];
+        _stationName = properties[@"name"];
+        _totalDocks = [properties[@"totalDocks"] integerValue];
+    }
+}
+
+- (void)_setUpForNonPhiladelphiaStationWithJSON:(NSDictionary *)json
+{
+    _stationID = json[@"id"];
+    _docksAvailable = [json[@"availableDocks"] integerValue];
+    _totalDocks = [json[@"totalDocks"] integerValue];
+    _coordinate = CLLocationCoordinate2DMake([json[@"latitude"] doubleValue], [json[@"longitude"] doubleValue]);
+    _inService = [json[@"statusValue"] isEqualToString:@"In Service"] && ![json[@"testStation"] boolValue];
+    _bikesAvailable = [json[@"availableBikes"] integerValue];
+}
+
 @end
