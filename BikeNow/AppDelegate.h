26f7528b90ab1b609848637a9c3c86ae573bf873
Mason Silber <mdsilber1@gmail.com>
2015-05-15 14:35:55 -0400

    Initial Commit

---
+++ BikeNow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BikeNow
+//
+//  Created by Mason Silber on 5/15/15.
+//  Copyright (c) 2015 Mason Silber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
